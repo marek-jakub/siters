86dfc83c0cd6cd428df2695b50669e007007a0d8
marek-jakub <band@tutamail.com>
2026-05-28 15:25:06 +0200

    Removed overshoot/undershoot GUI indicators.

--- src/main.c
+++ src/main.c
@@ -16,6 +16,14 @@ int main(int argc, char *argv[]) {
     
     gtk_init(&argc, &argv);
 
+    /* Remove GTK scrolled window overshoot/undershoot indicators (dashed lines at edges) */
+    GtkCssProvider *css_provider = gtk_css_provider_new();
+    gtk_css_provider_load_from_data(css_provider,
+        "scrolledwindow overshoot, scrolledwindow undershoot { background: none; }", -1, NULL);
+    gtk_style_context_add_provider_for_screen(gdk_screen_get_default(),
+        GTK_STYLE_PROVIDER(css_provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    g_object_unref(css_provider);
+
     GtkWidget *window = create_main_window();
     
     // Load saved state and apply to window
